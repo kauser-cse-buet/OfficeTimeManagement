8d757c5651a90ab9847b498feebf9bd3af1e5fd5
Md. Kauser Ahmmed <anik2137@gmail.com>
2017-10-27 18:08:23 -0500

    Update get_office_lock_unlock_time.py

--- get_office_lock_unlock_time.py
+++ get_office_lock_unlock_time.py
@@ -1,3 +1,5 @@
+# for more details follow the link: http://docs.activestate.com/activepython/3.3/pywin32/Windows_NT_Eventlog.html
+
 
 import win32evtlog
 readFlags = win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
